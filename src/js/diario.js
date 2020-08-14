dce534740c6566e93c0c34ffcd56ad4c0098667e
Julian Caceres <caceresf.julian@gmail.com>
2020-08-14 15:26:09 -0500

    Add forecast graph

--- src/js/diario.js
+++ src/js/diario.js
@@ -1,15 +1,36 @@
 const urlDaily = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 
-let forecast = []
+let forecast = [15, 17, 19, 14, 22, 24, 20, 18]
+
+var svg = d3.select("#forecast").append("svg")
+            .attr("height", "100%")
+            .attr("width", "100%");
+
+function forecastGraph(){
+    svg.selectAll("rect")
+        .data(forecast)
+        .enter().append("rect")
+            .attr("height", function(d){ return ((d - 10) * 8) })
+            .attr("width", "20")
+            .attr("x", function(d, i){return (i * 30) + 25})
+            .attr("y", function (d, i){return 220 - (d * 8)});  
+}
+
+forecastGraph();
 
 export const getDaily = async (jsonResponse, apikey, lang, units) => {
     try {
         const response2 = await fetch(urlDaily + jsonResponse.coord.lat + "&lon=" + jsonResponse.coord.lon + apikey + lang + units);
         if (response2.ok) {
+            forecast = [];
+            document.getElementById('forecast').remove
             const jsonResponse2 = await response2.json();
             console.log(jsonResponse2);
-            forecast.push(jsonResponse2.daily);
-            console.log(forecast[0][1].temp);
+            for (const [key, value] of Object.entries(jsonResponse2.daily)){
+                forecast.push(`${value.temp.day}`)
+            };
+            forecastGraph();
+            console.log(forecast);
             return jsonResponse2;
         } throw new Error('Request failed')
     } catch (error) {
