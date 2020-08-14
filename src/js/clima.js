3cc9cc5e11d514774d8c0b6f3e24c5e93027c7d1
Julian Caceres <caceresf.julian@gmail.com>
2020-08-13 22:32:19 -0500

    Adjust bg img

--- src/js/clima.js
+++ src/js/clima.js
@@ -1,16 +1,11 @@
 import { getDaily } from './diario.js';
 import { dateTime } from './time_date.js';
 import { setBackgroundImage } from './bg_img.js';
-import { setBackup } from './bg_img.js';
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-
 const apikey = '&appid=8de836df4e3363996ed5231aa642ea98';
-
 const units = '&units=metric';
-
 const lang = '&lang=es';
-
 const iconoSalida = document.getElementById('iconoSalida');
 
 export const getData = async () => {
@@ -24,7 +19,7 @@ export const getData = async () => {
       cityOutput.innerHTML = jsonResponse.name + ', ' + jsonResponse.sys.country;
       description.innerHTML = jsonResponse.weather[0].description;
       iconoSalida.src = 'http://openweathermap.org/img/wn/' + jsonResponse.weather[0].icon + '@2x.png';
-      setBackgroundImage(jsonResponse.weather[0].id).then(setBackup());
+      setBackgroundImage(jsonResponse.weather[0].id);
       getDaily(jsonResponse, apikey, lang, units);
       return jsonResponse;
     } throw new Error('Request failed!')
