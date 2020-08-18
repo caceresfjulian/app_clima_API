01daed95167404119b1d7b0255fb357624a908f5
Julian Caceres <caceresf.julian@gmail.com>
2020-08-18 11:02:01 -0500

    Set create line chart

--- src/js/diario.js
+++ src/js/diario.js
@@ -1,33 +1,46 @@
+import {dateTime} from './time_date.js';
+
 const urlDaily = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 
-let forecast = [15, 17, 19, 14, 22, 24, 20, 18]
+let forecast = [5, 4, 12, 8, 11, 7, 9, 6];
 
-function forecastGraph(){
-    var svg = d3.select("#forecast").append("svg")
-            .attr("id", "graph")
-            .attr("height", "100%")
-            .attr("width", "100%");
-    svg.selectAll("rect")
-        .data(forecast)
-        .enter().append("rect")
-            .text(function(d){return d})
-            .attr("class", "bar")
-            .attr("height", 
-                function(d){ return 140 - (Math.max(...forecast) - d + 3 ) * 8
-           })
-            .attr("width", "20")
-            .attr("x", function(d, i){return (i * 30) + 25})
-            .attr("y", function (d, i){return ((Math.max(...forecast) - d + 1) * 8) })
-    svg.selectAll("text")   
-        .data(forecast)
-        .enter()
-        .append("text")
-        .text(function(d){return `${Math.floor(d)}°`})
-            .attr("x", function(d, i){return (i * 30) + 25})        
-            .attr("y", function (d, i){return  145})
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 300 - margin.left - margin.right,
+    height = 150 - margin.top - margin.bottom;
+
+var svg = d3.select("#forecast")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+function createLineChart(data){
+    var x = d3.scaleTime()
+        .domain(d3.extent(data, function(d) { return d.time; }))
+        .range([ 0, width ]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+        
+      // Add Y axis
+        var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return +d.temp; })])
+        .range([ height, 0 ]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    svg.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", d3.line()
+        .x(function(d) { return x(d.time) })
+        .y(function(d) { return y(d.temp) })
+    )
 }
-
-forecastGraph();
 
 export const getDaily = async (jsonResponse, apikey, lang, units) => {
     try {
@@ -37,12 +50,10 @@ export const getDaily = async (jsonResponse, apikey, lang, units) => {
             document.getElementById('forecast').remove
             const jsonResponse2 = await response2.json();
             console.log(jsonResponse2);
-            for (const [key, value] of Object.entries(jsonResponse2.daily)){
-                forecast.push(`${value.temp.day}`)
+            for (const [key, value] of Object.entries(jsonResponse2.hourly)){
+                forecast.push({time: value.dt ,temp: value.temp})
             };
-            let graph = document.getElementById("graph");
-            graph.remove();
-            forecastGraph();
+            createLineChart(forecast);
             console.log(forecast);
             return jsonResponse2;
         } throw new Error('Request failed')
